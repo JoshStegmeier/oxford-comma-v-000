4afcca58fa3033f3a6be72690ea2fb881644c047
Josh Stegmeier <josh@steg.me>
2018-05-18 18:24:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   size = array.size
   if size > 2
     lastItem = array.pop
-    newStr = array.join(", ") + " and " + lastItem
+    newStr = array.join(", ") + ", and " + lastItem
   end
 end
